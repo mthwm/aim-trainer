b7744a26e1622baa7dc01d70e76761d5e78a96b8
mthwm <matthewm1@seznam.cz>
2022-01-11 20:01:28 +0100

    .

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+let randomInt = (max) => Math.floor(Math.random() * max);
+
+var currentScore = 0;
+
 let startGame = () => {
   const mainField = document.getElementById("main");
   const playButton = document.getElementById("playButton");
   const currentScoreContainer = document.getElementById("currentScore");
-
-  currentScoreContainer.innerHTML = 0;
+  currentScoreContainer.innerHTML = currentScore;
   mainField.removeChild(playButton);
+  newTarget();
 };
 
-let newTarget = (top, left) => {
-  const target = document.createElement(div);
+let newTarget = () => {
+  const currentScoreContainer = document.getElementById("currentScore");
+  const target = document.createElement("div");
+  target.className = "target";
+  target.style.top += randomInt(90) + "%";
+  target.style.left += randomInt(90) + "%";
+  const mainField = document.getElementById("main");
+  target.addEventListener("click", () => {
+    currentScore++;
+    currentScoreContainer.innerHTML = currentScore;
+    newTarget();
+    mainField.removeChild(target);
+  });
+  setTimeout(() => {
+    target.style.transform = "scale(0)";
+  }, 700);
+
+  mainField.appendChild(target);
 };
