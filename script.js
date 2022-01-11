3a2c7079355abc0b75ad5dc5e73c46b93c7042bc
mthwm <matthewm1@seznam.cz>
2022-01-11 15:37:53 +0100

    .

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+let startGame = () => {
+  const mainField = document.getElementById("main");
+  const playButton = document.getElementById("playButton");
+  const currentScoreContainer = document.getElementById("currentScore");
+
+  currentScoreContainer.innerHTML = 0;
+  mainField.removeChild(playButton);
+};
+
+let newTarget = (top, left) => {
+  const target = document.createElement(div);
+};
