5f1c106930d063b88076a5d389c4be19c73dd0a4
mthwm <matthewm1@seznam.cz>
2022-01-12 21:31:49 +0100

    .

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 let randomInt = (max) => Math.floor(Math.random() * max);
 
-var currentScore = 0;
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
 
-let startGame = () => {
-  const mainField = document.getElementById("main");
-  const playButton = document.getElementById("playButton");
-  const currentScoreContainer = document.getElementById("currentScore");
-  currentScoreContainer.innerHTML = currentScore;
-  mainField.removeChild(playButton);
-  newTarget();
-};
+// var currentScore = 0;
 
-let newTarget = () => {
-  const currentScoreContainer = document.getElementById("currentScore");
-  const target = document.createElement("div");
-  target.className = "target";
-  target.style.top += randomInt(90) + "%";
-  target.style.left += randomInt(90) + "%";
-  const mainField = document.getElementById("main");
-  target.addEventListener("click", () => {
-    currentScore++;
-    currentScoreContainer.innerHTML = currentScore;
-    newTarget();
-    mainField.removeChild(target);
-  });
-  setTimeout(() => {
-    target.style.transform = "scale(0)";
-  }, 700);
+// async function startGame() {
+//   const mainField = document.getElementById("main");
+//   const playButton = document.getElementById("playButton");
+//   const currentScoreContainer = document.getElementById("currentScore");
+//   currentScoreContainer.innerHTML = currentScore;
+//   mainField.removeChild(playButton);
+//   newTarget();
+// }
 
-  mainField.appendChild(target);
-};
+//
+
+//
+
+// function newTarget() {
+//   const currentScoreContainer = document.getElementById("currentScore");
+//   let continuePlaying = false;
+//   const target = document.createElement("div");
+//   target.className = "target";
+//   target.style.top += randomInt(90) + "%";
+//   target.style.left += randomInt(90) + "%";
+//   const mainField = document.getElementById("main");
+//   mainField.appendChild(target);
+//   target.addEventListener("click", () => {
+//     currentScore++;
+//     continuePlaying = true;
+//     currentScoreContainer.innerHTML = currentScore;
+//     fetch("https://api.countapi.xyz/update/Super_Stranka/ZULUL/?amount=1");
+//     newTarget();
+//     mainField.removeChild(target);
+//   });
+//   return continuePlaying;
+// }
+
+// function gameOver() {
+//   if (x == false) {
+//     console.log("game over");
+//   }
+// }
