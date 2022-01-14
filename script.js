ce9cb53309ca0efd9aa59c0066983c4285075d3d
mthwm <89951259+mthwm@users.noreply.github.com>
2022-01-14 20:33:20 +0100

    .

--- script.js
+++ script.js
@@ -1,62 +1,33 @@
 let randomInt = (max) => Math.floor(Math.random() * max);
 
-let startGame = () => {
-  const playButton = document.getElementById("playButton");
-  const mainField = document.getElementById("main");
+var currentScore = 0;
 
+let startGame = () => {
+  const mainField = document.getElementById("main");
+  const playButton = document.getElementById("playButton");
+  const currentScoreContainer = document.getElementById("currentScore");
+  currentScoreContainer.innerHTML = currentScore;
   mainField.removeChild(playButton);
+  newTarget();
 };
 
-// function sleep(ms) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
+let newTarget = () => {
+  const currentScoreContainer = document.getElementById("currentScore");
+  const target = document.createElement("div");
+  target.className = "target";
+  target.style.top += randomInt(90) + "%";
+  target.style.left += randomInt(90) + "%";
+  const mainField = document.getElementById("main");
+  target.addEventListener("click", () => {
+    currentScore++;
+    fetch("https://api.countapi.xyz/update/Super_Stranka/ZULUL/?amount=1");
+    currentScoreContainer.innerHTML = currentScore;
+    newTarget();
+    mainField.removeChild(target);
+  });
+  setTimeout(() => {
+    target.style.transform = "scale(0)";
+  }, 700);
 
-// var currentScore = 0;
-
-// let newTarget = () => {
-//   const currentScoreContainer = document.getElementById("currentScore");
-//   const target = document.createElement("div");
-//   target.className = "target";
-//   target.style.top += randomInt(90) + "%";
-//   target.style.left += randomInt(90) + "%";
-//   const mainField = document.getElementById("main");
-//   target.addEventListener("click", () => {
-//     currentScore++;
-//     fetch("https://api.countapi.xyz/update/Super_Stranka/ZULUL/?amount=1");
-//     currentScoreContainer.innerHTML = currentScore;
-//     newTarget();
-//     mainField.removeChild(target);
-//   });
-//   setTimeout(() => {
-//     target.style.transform = "scale(0)";
-//   }, 700);
-
-//
-
-//
-
-// function newTarget() {
-//   const currentScoreContainer = document.getElementById("currentScore");
-//   let continuePlaying = false;
-//   const target = document.createElement("div");
-//   target.className = "target";
-//   target.style.top += randomInt(90) + "%";
-//   target.style.left += randomInt(90) + "%";
-//   const mainField = document.getElementById("main");
-//   mainField.appendChild(target);
-//   target.addEventListener("click", () => {
-//     currentScore++;
-//     continuePlaying = true;
-//     currentScoreContainer.innerHTML = currentScore;
-//     fetch("https://api.countapi.xyz/update/Super_Stranka/ZULUL/?amount=1");
-//     newTarget();
-//     mainField.removeChild(target);
-//   });
-//   return continuePlaying;
-// }
-
-// function gameOver() {
-//   if (x == false) {
-//     console.log("game over");
-//   }
-// }
+  mainField.appendChild(target);
+};
